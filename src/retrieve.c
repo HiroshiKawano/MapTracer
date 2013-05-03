6c38921172795ad8a729fee329a7e6f91af060a4
yishii <ishii.yasuhiro@gmail.com>
2013-05-03 12:47:43 +0000

    format adjusted

--- src/retrieve.c
+++ src/retrieve.c
@@ -26,98 +26,91 @@ static size_t retriever_header_callback(char* ptr,size_t size,size_t nmemb,void*
 
 static size_t retriever_write_callback(char* ptr,size_t size,size_t nmemb,void* userdata)
 {
-  int ret = nmemb*size;
-  static int current_pos = 0;
-
-  if(content_buffer == NULL){
-    if(current_content_size != -1 && current_content_size < CONTENT_MAX_SIZE){
-      content_buffer = malloc(current_content_size);
-      current_pos = 0;
-      if(content_buffer == NULL){
-	printf("%s : memory allocation error\n",__func__);
-	ret = 0;
-      }
+    int ret = nmemb*size;
+    static int current_pos = 0;
+    
+    if(content_buffer == NULL){
+	if(current_content_size != -1 && current_content_size < CONTENT_MAX_SIZE){
+	    content_buffer = malloc(current_content_size);
+	    current_pos = 0;
+	    if(content_buffer == NULL){
+		printf("%s : memory allocation error\n",__func__);
+		ret = 0;
+	    }
+	}
     }
-  }
-
-  if(content_buffer != NULL){
-    memcpy(content_buffer+current_pos,ptr,nmemb*size);
-    current_pos += size*nmemb;
+    
+    if(content_buffer != NULL){
+	memcpy(content_buffer+current_pos,ptr,nmemb*size);
+	current_pos += size*nmemb;
 #ifdef DEBUG
-    printf("curpos = %d\n",current_pos);
+	printf("curpos = %d\n",current_pos);
 #endif
-  }
-
-  return(ret);
+    }
+    
+    return(ret);
 }
 
 static size_t retriever_header_callback(char* ptr,size_t size,size_t nmemb,void* userdata)
 {
-  // Memo:
-  // parse header with alternatively allocated "buff" to handle it as like MS's _snscanf()
-
-  char* buff = malloc(size*nmemb+1);
-  int len = strlen(HTTP_HEADER_CONTENT_LENGTH);
-
-  if(buff == NULL){
-    printf("%s : Memory allocation error\n",__func__);
-    return(-1);
-  }
-  memcpy((void*)buff,(const void*)ptr,size*nmemb);
-  buff[size*nmemb] = 0; // add terminator
-
-  if(strncmp(HTTP_HEADER_CONTENT_LENGTH,buff,len) == 0){
-    sscanf(buff+len,"%d",&current_content_size);
-    printf("CONTENT-SIZE = %ld\n",current_content_size);
-  }
-
-  free(buff);
-
-  return(nmemb*size);
+    // Memo:
+    // parse header with alternatively allocated "buff" to handle it as like MS's _snscanf()
+    
+    char* buff = malloc(size*nmemb+1);
+    int len = strlen(HTTP_HEADER_CONTENT_LENGTH);
+    
+    if(buff == NULL){
+	printf("%s : Memory allocation error\n",__func__);
+	return(-1);
+    }
+    memcpy((void*)buff,(const void*)ptr,size*nmemb);
+    buff[size*nmemb] = 0; // add terminator
+    
+    if(strncmp(HTTP_HEADER_CONTENT_LENGTH,buff,len) == 0){
+	sscanf(buff+len,"%d",&current_content_size);
+	printf("CONTENT-SIZE = %ld\n",current_content_size);
+    }
+    
+    free(buff);
+    
+    return(nmemb*size);
 }
 
 
 bool http_retrieve(unsigned char* url)
 {
-  CURL *curl;
-  CURLcode res;
-
-  curl = curl_easy_init();
-  if(curl) {
-    current_content_size = -1;
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &retriever_write_callback);
-    curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, &retriever_header_callback);
-
-    /* Perform the request, res will get the return code */
-    res = curl_easy_perform(curl);
-    /* Check for errors */
-    if(res != CURLE_OK){
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
-    } else {
-      
-      {
-	FILE* f;
-	f = fopen("out.png","wb");
-	fwrite(content_buffer,current_content_size,1,f);
-	fclose(f);
-      }
-
-      free(content_buffer);
-    }
-    /* always cleanup */
-    curl_easy_cleanup(curl);
-  }
-  return true;
-}
+    CURL *curl;
+    CURLcode res;
+    
+    curl = curl_easy_init();
+    if(curl) {
+	current_content_size = -1;
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &retriever_write_callback);
+	curl_easy_setopt(curl, CURLOPT_HEADERFUNCTION, &retriever_header_callback);
+	
+	/* Perform the request, res will get the return code */
+	res = curl_easy_perform(curl);
+	/* Check for errors */
+	if(res != CURLE_OK){
+	    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+		    curl_easy_strerror(res));
+	} else {
 
 #if 0
-int main(void)
-{
-  http_retrieve(
-		"http://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x600&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Ccolor:red%7Clabel:C%7C40.718217,-73.998284&sensor=false&format=png"
-		);
-}
+	    {
+		FILE* f;
+		f = fopen("out.png","wb");
+		fwrite(content_buffer,current_content_size,1,f);
+		fclose(f);
+	    }
 #endif
+	    free(content_buffer);
+	}
+	/* always cleanup */
+	curl_easy_cleanup(curl);
+    }
+    return true;
+}
+
